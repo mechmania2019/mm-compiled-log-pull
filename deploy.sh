928233a4cbca51c15b31e58e667afb2bb56e7473
Pranay Prakash <pranay.gp@gmail.com>
2018-09-20 17:52:15 -0500

    Remove deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build . -t gcr.io/mechmania2017/compiled-log-pull:latest
-docker push gcr.io/mechmania2017/compiled-log-pull:latest
-kubectl apply -f app.yaml
-kubectl delete pods -l app=compiled-log-pull
