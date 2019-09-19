eec5c3153278828c601f1a7ba289aa7e800550cc
angiecheng <angie.cheng33@gmail.com>
2019-09-19 18:37:27 -0500

    can now check logs of user chosen version

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const { Script } = require('mm-schemas')(mongoose)
 
 const send = (res, status, data) => (res.statusCode = status, res.end(data));
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.set('useCreateIndex', true);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 mongoose.Promise = global.Promise;
 
 AWS.config.update({
   'accessKeyId': process.env.MM_AWS_SECRET_KEY_ID,
   'secretAccessKey': process.env.MM_AWS_SECRET_ACCESS_KEY,
-  'bucketname': "mechmania2019"
 });
+
+
+
 const s3 = new AWS.S3({
   params: { Bucket: "mechmania2019" }
 });
@@ -23,19 +24,36 @@ module.exports = authenticate(async (req, res) => {
   console.log(`${team.name} - Getting the compiled log file from S3`);
   if (!team.mostRecentPush) {
     send(res, 404, "You haven't uploaded any bots yet using `mm push`");
+    return;
   }
-  const script = await Script.findById(team.mostRecentPush).exec()
 
-  console.log("script " + script.key)
-  const data = s3
-    .getObject({ Key: `compiled/${script.key}` })
-    .createReadStream()
-    .on("error", error => {
-      console.log(error);
-      send(res, 202, "Not Ready Yet");
-    });
-  console.log("script " + script.key)
+  if (req.url === "/") {
+    console.log(team);
+    const script = await Script.findbyId(team.latestScript).exec();
+    const data = s3
+      .getObject({ Key : `compiled/${script}` })
+      .createReadStream()
+      .on("error", error => {
+        console.log(error);
+        send(res, 202, "Not Ready Yet");
+      });
+      res.statusCode = 200;
+      data.pipe(res);
+  } else {
+    const script = req.url.slice(1);
 
-  res.statusCode = 200;
-  data.pipe(res);
+    console.log(`compiled/${script}`);
+    const data = s3
+      .getObject({ Key: `compiled/${script}` })
+      .createReadStream()
+      .on("error", error => {
+        console.log(error);
+        send(res, 202, "Not Ready Yet");
+      });
+    console.log("script " + script);
+    console.log(data);
+
+    res.statusCode = 200;
+    data.pipe(res);
+  }
 });
