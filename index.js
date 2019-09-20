de867891bc6210e7a8a41366ab7636a127a37928
angiecheng <angie.cheng33@gmail.com>
2019-09-19 21:59:14 -0500

    removed unnecessary loging

--- index.js
+++ index.js
@@ -13,13 +13,12 @@ AWS.config.update({
   'secretAccessKey': process.env.MM_AWS_SECRET_ACCESS_KEY,
 });
 
-
-
 const s3 = new AWS.S3({
   params: { Bucket: "mechmania2019" }
 });
 
 module.exports = authenticate(async (req, res) => {
+
   const team = req.user;
   console.log(`${team.name} - Getting the compiled log file from S3`);
   if (!team.mostRecentPush) {
@@ -31,14 +30,14 @@ module.exports = authenticate(async (req, res) => {
     console.log(team);
     const script = await Script.findbyId(team.latestScript).exec();
     const data = s3
-      .getObject({ Key : `compiled/${script}` })
+      .getObject({ Key: `compiled/${script}` })
       .createReadStream()
       .on("error", error => {
         console.log(error);
         send(res, 202, "Not Ready Yet");
       });
-      res.statusCode = 200;
-      data.pipe(res);
+    res.statusCode = 200;
+    data.pipe(res);
   } else {
     const script = req.url.slice(1);
 
@@ -51,7 +50,6 @@ module.exports = authenticate(async (req, res) => {
         send(res, 202, "Not Ready Yet");
       });
     console.log("script " + script);
-    console.log(data);
 
     res.statusCode = 200;
     data.pipe(res);
